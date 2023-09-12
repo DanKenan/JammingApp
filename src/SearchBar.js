2b97b9786f6e60b0fcd9f501b11d080432d555b1
DanKenan <122852728+DanKenan@users.noreply.github.com>
2023-09-11 23:52:46 -0400

    Search bar design

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import Select from 'react-select';
+import { useState } from 'react';
 import './CSS/NavBar.css';
 
+
+/*still need: 
+1)when click change color.
+2)multiple choice option. 
+3)create a playlist viewer
+4)when submit to show it in the playlist viewer.
+
+ style:
+1) make search bar less wide. 
+2} move evrything to the middle of the page.
+*/
+
 export default function SearchBar(props) {
+  const [selectedOption, setSelectedOption] = useState(null);
+
   const songOptions = Object.keys(props.songs).map((songKey) => {
     const { artist, song, img } = props.songs[songKey];
   return {
@@ -13,32 +28,62 @@ export default function SearchBar(props) {
 })
 ;
 
-
-      /*const songOptions = [
-        { value: `${songsCheck.song1.artist} - ${songsCheck.song1.song}`, label: `${songsCheck.song1.artist} - ${songsCheck.song1.song}`, image: songsCheck.song1.img },
-        { value: `${songsCheck.song2.artist} - ${songsCheck.song2.song}`, label: `${songsCheck.song2.artist} - ${songsCheck.song2.song}`, image: songsCheck.song2.img },
-        { value: `${songsCheck.song3.artist} - ${songsCheck.song3.song}`, label: `${songsCheck.song3.artist} - ${songsCheck.song3.song}`, image: songsCheck.song3.img }
-      ];*/
-    
-      const CustomOption = ({ innerProps, label, data }) => (
-        <div {...innerProps} className='custom-option'>
-          <img src={data.image} alt="" className="navImage" />
+      const customOption = ({ innerProps, label, data, isSelected }) => (
+        <div {...innerProps} className={`custom-option ${isSelected ? 'selected' : ''}`}>
+          <img src={data.image} alt="Singer Picture" className="navImage" />
           <span className='song-label'>{label}</span>
         </div>
       );
-  
-  return (
-    <form>
-      <h3 className='heading'>Jewish Music</h3>
-      <div className='Image'></div>
-      <label htmlFor="songName" className='formText'>Start your search</label>
-      <Select
-        options={songOptions}
-        placeholder="Song name"
-        components={{ Option: CustomOption }}
-      />
- 
-    </form>
-  );
+
+
+
+      const customStyles = { 
+        control: (styles) => ({ ...styles, 
+          backgroundColor: "pink", 
+          color: "black",
+          width: '100%',
+        }),
+        option: (provided, state) => ({
+          ...provided,
+          backgroundColor: state.isSelected ? 'red' : 'white',
+          cursor: 'pointer',
+        }),
+        multiValue: (provided, state) => ({
+          ...provided,
+          backgroundColor: "yellow",
+          color: "black",
+        }),
+        multiValueLabel: (provided) => ({
+          ...provided,
+          color: "red",
+        }),
+      }; 
+
+      const handleOptionSelect = (selected) => {
+        setSelectedOption(selected);
+      };
+      
+      return (
+        <div className='search-bar'>
+        <form className='navForm'>
+          <h3 className='heading'>Jewish Music</h3>
+          <div className='Image'></div>
+          <label htmlFor="songName" className='formText'>Start your search</label>
+          <Select
+            options={songOptions}
+            placeholder="Song name"
+            components={{ Option: customOption }}
+            styles={customStyles}
+            isMulti
+            value={selectedOption}
+            onChange={handleOptionSelect}
+            closeMenuOnSelect={false}
+            hideSelectedOptions={false}
+          />
+          <input type='submit' value='Submit'/>
+        </form>
+        </div>
+      );
+      
   }
 
